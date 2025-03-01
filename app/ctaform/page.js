e9ad5f5e3cba870fae4195f4c43be2276b594b14
mabgcm <bugucam@gmail.com>
2025-03-01 00:56:29 -0500

    api link

--- app/ctaform/page.js
+++ app/ctaform/page.js
@@ -23,7 +23,7 @@ export default function CtaFormPage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('api/submit-form/route.js', {
+            const response = await fetch('app/api/submit-form/route.js', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
